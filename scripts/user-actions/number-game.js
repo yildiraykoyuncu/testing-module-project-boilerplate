41af1e576891c6afd2575911de36038b431cfeb9
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-07-25 14:11:06 +0200

    added html and DOM close #2

--- scripts/user-actions/number-game.js
+++ scripts/user-actions/number-game.js
@@ -1,6 +1,27 @@
 //counts the number attempts to calculate score
 let attempts = 0;
 
+//score
+
+let score = 0;
+
+//Displays the score
+let scoreDisplay = document.querySelector('#score')
+scoreDisplay.innerHTML = 'Your score is:'
+
+
+//Displays the message
+
+let messageDisplay = document.querySelector('#message')
+messageDisplay.innerHTML = 'Enter a number and click guess button'
+
+
+//Random number
+
+let compNumber = 0
+localStorage.setItem('compNumber', compNumber)
+console.log(localStorage.getItem('compNumber'))
+
 
 //generates a random number within a range.
 const randomNumberGenerator = (start, end) => {
@@ -8,63 +29,74 @@ const randomNumberGenerator = (start, end) => {
 
 };
 
-//checks if users guess is equals to randomly generate number; 
-const NumberChecker = (guess, compNumber) => {
-    if (guess === compNumber) {
-        alert('Correct! Congratulations!')
-        return;
-    } else {
-        let userInput = prompt('Nope! Try again!');
-        if (userInput === null) { return };
-
-        let userGuess = Number(userInput);
-
-        attempts += 1;
-        NumberChecker(userGuess, compNumber);
-    }
-};
-
 //calculates the final score depending on number of failed attempts;
 
 const scoreCalculator = (attempts) => {
 
-    let score = (100 - (attempts * 10));
+    score = (100 - (attempts * 10));
 
     if (score <= 0) {
-        alert('Your score is 0')
+        scoreDisplay.innerHTML = 'Your score is 0'
     } else {
-        alert(`Your score is ${score}`)
+        scoreDisplay.innerHTML = `Your score is ${score}`
     }
 
 }
 
+
+//checks if users guess is equals to randomly generate number; 
+const NumberChecker = (guess, compNumber) => {
+    if (guess === compNumber) {
+        messageDisplay.innerHTML = 'Correct! Congratulations!'
+        scoreCalculator(attempts)
+        return;
+    } else {
+        messageDisplay.innerHTML = 'Nope! Try Again!'
+        attempts += 1;
+
+    };
+}
+
+
+
+
+
 //handler functions starts the sequence after button click and calls the functions in a order.
-const numberGameHandler = () => {
+const settingsHandler = () => {
 
     //resets number of attempts at the beginning of the game.
     attempts = 0;
 
-    let start = prompt('Set a starting point');
+    //resets the score
+    scoreDisplay.innerHTML = 'Your score is:'
+
+    //resets the message
+    messageDisplay.innerHTML = 'Enter a number and click guess button'
+
+    let start = document.querySelector('#start').value;
     if (start === null) { return };
 
-    let end = prompt('set an ending point');
+    let end = document.querySelector('#end').value;
     if (end === null) { return };
 
     let startingNumber = Number(start)
 
     let endingNumber = Number(end)
 
-    let compNumber = randomNumberGenerator(startingNumber, endingNumber);
+    compNumber = randomNumberGenerator(startingNumber, endingNumber);
+    console.log(compNumber)
+    console.log(localStorage.getItem('compNumber'))
 
-    let userNumber = prompt(`I have number in mind between ${start} and ${end}. Can you guess it?`);
+}
+
+const numberGameHandler = () => {
+    let userNumber = document.querySelector('#guess').value;
     if (userNumber === null) { return };
 
     let guess = Number(userNumber)
 
     NumberChecker(guess, compNumber);
 
-    scoreCalculator(attempts)
-}
 
+}
 let random = randomNumberGenerator(40, 50)
-console.log(random)
